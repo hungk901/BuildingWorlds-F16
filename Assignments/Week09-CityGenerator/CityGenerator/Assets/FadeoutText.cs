e774e5a0de21c88d25504cb28ec9caa5268aecfb
Ray Hung <hungk901@newschool.edu>
2016-11-04 12:24:28 -0400

    Update

---
+++ Assignments/Week09-CityGenerator/CityGenerator/Assets/FadeoutText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeoutText : MonoBehaviour {
+
+	Text CityNameText;
+	float colorAlpha = 1f;
+
+	void Start () {
+		CityNameText = GameObject.Find ("CityName").GetComponent<Text> ();
+
+	}
+
+	void Update () {
+		StartCoroutine (turnOffText ());
+	}
+
+	IEnumerator turnOffText () {
+		yield return new WaitForSeconds (3);
+		colorAlpha -= .075f;
+		CityNameText.color = new Color(CityNameText.color.r, CityNameText.color.g, CityNameText.color.b, colorAlpha);
+	}
+}
--- Assignments/Week09-CityGenerator/CityGenerator/Assets/Scripts/Streetmaker.cs
+++ Assignments/Week09-CityGenerator/CityGenerator/Assets/Scripts/Streetmaker.cs
@@ -8,30 +8,34 @@ public class Streetmaker : MonoBehaviour {
 	public GameObject myStreetmakerPrefab;
 	public GameObject myBuildingPrefab01;
 	public GameObject myBuildingPrefab02;
+	GameObject tmpPrefab01;
+	GameObject tmpPrefab02;
 	public int stepCount = 0;
+	public int stepCountMax = 0;
+
+	void Start () {
+		stepCountMax = (int)Random.Range (15f, 35f);
+	}
 
 	void Update () {
-
 		transform.position = transform.position + transform.forward * 5f;
 
-		GameObject tmpPrefab01 = RandomBuilding ();
-		GameObject tmpPrefab02 = RandomBuilding ();
-
+		tmpPrefab01 = RandomBuilding ();
+		tmpPrefab02 = RandomBuilding ();
 		float randomNumTmp1 = Random.Range (0f, 100f);
 
-		if (randomNumTmp1 < 90f) {
+		if (randomNumTmp1 < 85f) {
 
 			GameObject leftBuilding = (GameObject)Instantiate (tmpPrefab01, transform.position - transform.right * 5f, transform.rotation);
-			leftBuilding.transform.localScale = new Vector3 (5f, Random.Range (1f, 5f), 5f);
+			leftBuilding.transform.localScale = new Vector3 (.9f, Random.Range (.5f, 4.5f), .9f);
 			leftBuilding.transform.position = new Vector3 (leftBuilding.transform.position.x, 0, leftBuilding.transform.position.z);
 			leftBuilding.GetComponent<Renderer> ().material.color = Random.ColorHSV ();
 
 			GameObject rightBuilding = (GameObject)Instantiate (tmpPrefab02, transform.position + transform.right * 5f, transform.rotation);
-			rightBuilding.transform.localScale = new Vector3 (5f, Random.Range (1f, 5f), 5f);
+			rightBuilding.transform.localScale = new Vector3 (.9f, Random.Range (.5f, 4.5f), .9f);
 			rightBuilding.transform.position = new Vector3 (rightBuilding.transform.position.x, 0, rightBuilding.transform.position.z);
 			rightBuilding.GetComponent<Renderer> ().material.color = Random.ColorHSV ();
-		} 
-		else {
+		} else {
 			float randomNumTmp2 = Random.Range (0f, 100f);
 
 			if (randomNumTmp2 < 50f) {
@@ -40,10 +44,9 @@ public class Streetmaker : MonoBehaviour {
 				Instantiate (myStreetmakerPrefab, transform.position, Quaternion.Euler (0, transform.eulerAngles.y + 90, 0));
 			}
 		}
-
 		stepCount++;
 
-		if (stepCount > 10) {
+		if (stepCount > stepCountMax) {
 			DestroyObject (this.gameObject);
 		}
 	}
@@ -52,7 +55,7 @@ public class Streetmaker : MonoBehaviour {
 		float randomNumForBuilding = Random.Range (0f, 100f);
 		GameObject _tmpPrefab;
 
-		if (randomNumForBuilding > 50f) {
+		if (randomNumForBuilding > 30f) {
 			_tmpPrefab = myBuildingPrefab01;
 		} else {
 			_tmpPrefab = myBuildingPrefab02;
